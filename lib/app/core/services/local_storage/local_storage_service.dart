12099106b54eee16ce3683da25db83040e80b2fe
Alba <alba__22@outlook.com>
2022-12-17 20:38:16 -0300

    start v3 by deleting almost all old things

--- lib/app/core/services/local_storage/local_storage_service.dart
+++
@@ -1,7 +0,0 @@
-abstract class LocalStorageService {
-  Future<String?> read(String key);
-  Future<void> delete(String key);
-  Future<void> write(String key, String value);
-  Future<void> clear();
-  Future<bool> contains(String key);
-}
